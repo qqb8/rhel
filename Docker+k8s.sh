bfcff79bb874f33da3188e79f34ed8b5b1cc3f08
Andrey.W11 <sharapov.an@internet.ru>
2023-06-04 19:57:56 +0500

    add docker+kubeadm

--- Docker+k8s.sh
+++ Docker+k8s.sh
@@ -25,7 +25,7 @@ systemctl enable --now cri-docker.socket
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\\\$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
