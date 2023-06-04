baaa89c93e509981bff66541e9812970a7af742d
Andrey.W11 <sharapov.an@internet.ru>
2023-06-05 00:11:39 +0500

    mod sh

--- sh/Docker+k8s.sh
+++ sh/Docker+k8s.sh
@@ -1,14 +1,22 @@
 # alma-linux 9.2
 
 cat > install-docker-ce8.sh << EOF2
+
 #!/bin/bash
+
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-dnf install -y git golang-bin docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf install -y git tar golang-bin docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker
+
 iptables -P FORWARD ACCEPT
 swapoff -a
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 git clone https://github.com/Mirantis/cri-dockerd.git
 curl -O https://storage.googleapis.com/golang/getgo/installer_linux
 chmod +x ./installer_linux
@@ -24,6 +32,7 @@ sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/syst
 systemctl daemon-reload
 systemctl enable cri-docker.service
 systemctl enable --now cri-docker.socket
+
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -35,5 +44,6 @@ exclude=kubelet kubeadm kubectl
 EOF
 dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 systemctl enable --now kubelet
+
 EOF2
 chmod +x install-docker-ce8.sh
